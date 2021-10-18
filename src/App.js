09d277e56e2e0cc1318487e5815e4a2209256bb2
Tang Yun <ntutangyun@gmail.com>
2021-10-18 23:04:54 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+/* eslint-disable no-undef */
 import "./App.css";
 import Header from "./Header";
+import {useEffect} from "react";
 
 const styles = {};
 
 function App() {
+
+    useEffect(() => {
+
+    }, []);
+
     return (
         <div className="App">
             <Header> </Header>
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,50 @@
+/* eslint-disable no-undef */
+import React from "react";
+
 const styles = {};
 
+function readFile(file) {
+    return new Promise(resolve => {
+        const reader = new FileReader();
+        reader.onload = (evt) => {
+            resolve(evt.target.result);
+        };
+        reader.readAsText(file);
+    });
+}
+
 function Header() {
+    const onProtobufLibsSelected = async (e) => {
+        console.log(e.target.files);
+        console.log(Array.from(e.target.files));
+
+        const fileContentList = [];
+        for (const file of Array.from(e.target.files)) {
+            const fileContent = await readFile(file);
+            fileContentList.push(fileContent);
+        }
+
+        console.log(fileContentList);
+
+        fileContentList.forEach(fileContent => {
+            eval(fileContent);
+        });
+
+        console.log(proto);
+    };
+
     return (
         <header className={"flex-row justify-content-between"}>
             <h1>Apollo Cyber Record Viewer</h1>
-            <input type="file"/>
+            <label>
+                import all protobuf_out files
+                <input type="file" multiple onChange={onProtobufLibsSelected}/>
+            </label>
+            <label>
+                Select recording file
+                <input type="file"/>
+            </label>
+
         </header>
     );
 }
